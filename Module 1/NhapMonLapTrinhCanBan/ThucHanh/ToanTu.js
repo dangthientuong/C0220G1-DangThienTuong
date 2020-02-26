7d103fb9b72620d5ec342746e3cd05d82adc5fa3
dangthientuong <tuongdesign@gmail.com>
2020-02-26 17:01:07 +0700

    abc

---
+++ Module 1/NhapMonLapTrinhCanBan/ThucHanh/ToanTu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("nhap chieu dai");
+inputHeight = prompt("nhap chieu cao");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is: " + area);
