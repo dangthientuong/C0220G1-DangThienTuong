9e1b32c2753250e81f2e93980267ee9fbb2bb1c4
dangthientuong <tuongdesign@gmail.com>
2020-02-26 17:00:07 +0700

    abc

---
+++ Module 1/NhapMonLapTrinhCanBan/BaiTap/baitapbien.js
@@ -0,0 +1,8 @@
+let inputVatly;
+let inputHoahoc;
+let inputSinhhoc;
+inputVatly = prompt("Nhap diem mon Vat ly");
+inputHoahoc = prompt("Nhap diem mon Hoa Hoc");
+inputSinhhoc = prompt("Nhap diem mon Sinh Hoc");
+let tong = Vatly + Hoahoc + Sinhhoc;
+document.write("The tong is" + tong);
