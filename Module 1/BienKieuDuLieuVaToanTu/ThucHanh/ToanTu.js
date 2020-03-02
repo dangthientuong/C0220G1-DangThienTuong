ad62fb39142d7c84a09f18c8183718c9e2a4f9e1
dangthientuong <tuongdesign@gmail.com>
2020-03-02 14:12:58 +0700

    abc

--- Module 1/BienKieuDuLieuVaToanTu/ThucHanh/ToanTu.js
+++
@@ -1,8 +0,0 @@
-let inputWidth;
-let inputHeight;
-inputWidth = prompt("nhap chieu dai");
-inputHeight = prompt("nhap chieu cao");
-let width = parseInt(inputWidth);
-let height = parseInt(inputHeight);
-let area = width * height;
-document.write("The area is: " + area);
